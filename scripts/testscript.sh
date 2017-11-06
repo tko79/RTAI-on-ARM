0801a82e632ebc513d7c6e454e99ed8bd92b8a2e
Torsten Koschorrek <devel@koschorrek.com>
2017-11-06 22:44:39 +0100

    scripts: testscript.sh: 3.1 load_unload_kernel_modules() added
    Signed-off-by: Torsten Koschorrek <devel@koschorrek.com>

--- scripts/testscript.sh
+++ scripts/testscript.sh
@@ -26,3 +26,51 @@ as published by the Free Software Foundation, either version 2 of the License,
 or (at your option) any later version.
 EOF
 )
+
+PREFIX_KMOD="/install/modules/"
+
+function _create_device_files() {
+    if [ ! -e /dev/rtf0 ]; then
+	mknod /dev/rtf0 c 150 0
+    fi
+    if [ ! -e /dev/rtf3 ]; then
+	mknod /dev/rtf3 c 150 3
+    fi
+}
+
+function _load_modules() {
+    sched_lxrt=$1
+
+    insmod ${PREFIX_KMOD}/rtai_hal.ko
+    insmod ${PREFIX_KMOD}/rtai_${sched_lxrt}.ko
+    insmod ${PREFIX_KMOD}/rtai_msg.ko
+    insmod ${PREFIX_KMOD}/rtai_sem.ko
+    insmod ${PREFIX_KMOD}/rtai_mbx.ko
+    insmod ${PREFIX_KMOD}/rtai_fifos.ko
+    insmod ${PREFIX_KMOD}/rtai_shm.ko
+}
+
+function _unload_modules() {
+    rmmod latency_rt &> /dev/null
+    rmmod preempt_rt &> /dev/null
+    rmmod switches_rt &> /dev/null
+    rmmod rtai_shm rtai_fifos rtai_mbx rtai_sem rtai_msg &> /dev/null
+    rmmod rtai_sched rtai_hal &> /dev/null
+    rmmod rtai_lxrt rtai_hal &> /dev/null
+}
+
+function load_unload_kernel_modules() {
+    echo "3.1: load/unload kernel modules"
+
+    _create_device_files
+    _unload_modules
+
+    echo "3.1.1: load/unload kernel modules (sched)"
+    _load_modules sched; lsmod; _unload_modules; lsmod
+    echo "3.1.2: load/unload kernel modules (sched)"
+    _load_modules sched; lsmod; _unload_modules; lsmod
+    echo "3.1.3: load/unload kernel modules (lxrt)"
+    _load_modules lxrt;  lsmod; _unload_modules; lsmod
+    echo "3.1.4: load/unload kernel modules (lxrt)"
+    _load_modules lxrt;  lsmod; _unload_modules; lsmod
+}
